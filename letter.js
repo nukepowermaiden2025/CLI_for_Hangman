4b9cb96719c5ea3bdbe2eed921a5de22bb48bf19
Kourtney Reynolds <Kourtney.Reynolds@gmail.com>
2018-04-09 18:40:38 -0700

    added method for letter and underscore reveal

--- letter.js
+++ letter.js
@@ -7,19 +7,30 @@
 
 
 
-var LetterProcessor = function(letter,hasLetterBeenGuessed){
+var LetterProcessor = function(letter){
     this.letter = letter;
-    this.hasLetterBeenGuessed = hasLetterBeenGuessed;
+    this.hasLetterBeenGuessed = false;
 }
+
+var newLetter =  new LetterProcessor("l");
+console.log(newLetter);
+
 
 LetterProcessor.prototype.letterOrUnderscoreReveal = function(){
+    if(this.hasLetterBeenGuessed){
+        return this.letter;
+    }else{
+        return "_";
+    }
     //   * A function that returns the underlying character if the letter has been guessed, or a 
 //   * placeholder (like an underscore) if the letter has not been guessed
-
 };
 
-LetterProcessor.prototype.guessProcessing = function(){
-    //   * A function that takes a character as an argument and checks it against the
-//   *  underlying character, updating the stored boolean value to true if it was guessed correctly
 
-}
+console.log(newLetter.letter);
+console.log(newLetter.letterOrUnderscoreReveal());
+
+
+
+module.exports = LetterProcessor;
+
--- word.js
+++ word.js
@@ -1,1 +1,13 @@
+// **Word.js**: Contains a constructor, Word that depends on the Letter constructor. 
+// This is used to create an object representing the current word the user is attempting to
+//  guess. That means the constructor should define:
+
+// * An array of `new` Letter objects representing the letters of the underlying word
+
+// * A function that returns a string representing the word. This should call the 
+// function on each letter object (the first function defined in `Letter.js`) that displays
+//  the character or an underscore and concatenate those together.
+
+
 var require = require("./letter.js");
+
