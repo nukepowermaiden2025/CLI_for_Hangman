2c105aaa811d6d48ebc45c53bfc1fce419f1820e
Kourtney Reynolds <Kourtney.Reynolds@gmail.com>
2018-04-13 20:27:43 -0700

    Add the function the updates the letter into the display array

--- word.js
+++ word.js
@@ -32,17 +32,23 @@ var newWord = new CurrentWord("Puppy");
 
 console.log(newWord.makeLettersArrayOfLetterObjects());
 console.log(newWord);
-
+//This will find the letter in the current word array and then
+//if it is found it will set the empty letter object property has been guessed equal to true.
 CurrentWord.prototype.findLetter = function(letterGuess){
     for (let i =0; i < this.emptyLetterArray.length; i++){
         if(letterGuess ===this.emptyLetterArray[i].letter){
-            this.emptyLetterArray[i].hasLetterBeenGuessed = true;
-            
+            this.emptyLetterArray[i].hasLetterBeenGuessed = true;  
         }
     }
 };
-
-//Make a method prototye and updates letter than is guess by the user or underscore.
+console.log(newWord.findLetter)
+//Make a method prototye and updates letter that is guessed by the user or fill underscore.
+CurrentWord.prototype.updateWordDisplay = function(){
+    let wordDisplay = [];
+    for(let i=0; i<this.emptyLetterArray.length; i++){
+        wordDisplay.push(this.emptyLetterArray[i].letterOrUnderscoreReveal());//This is suppose to be showing the letter or the underscore to this new word display
+    }
+}
 //Display letter
 //Check is everything has been guess
 //export module
